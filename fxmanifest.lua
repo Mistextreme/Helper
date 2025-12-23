e7c5457d9bbadab9f3ae81df09f2afa332f91600
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-12-23 21:10:27 +0000

    Upload: fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,35 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Caticus'
+description 'Free Banking System - ESX/QBCore/QBox Compatible'
+website 'www.5Mservers.com'
+
+shared_scripts {
+    'config.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    '@oxmysql/lib/MySQL.lua',
+    'server/main.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/css/style.css',
+    'html/js/script.js'
+}
+
+escrow_ignore {
+    'config.lua'
+}
+
+
+dependency '/assetpacks'
